bbf77fa2324538492d82bd6ff0263ae831328833
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-08-05 17:15:50 -0500

    #29 Relaciones Polimorficas

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -6,40 +6,52 @@ use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
     public function run()
     {
-        //Vaciar la tabla
+        // Vaciar la tabla
         User::truncate();
 
         $faker = \Faker\Factory::create();
 
-        //Crear la misma clave para todos los usuarios
-        //conviene hacerlo antes del for para que el seeder
-        // no se vuelva lento
+        // Crear la misma clave para todos los usuarios
+        // conviene hacerlo antes del for para que el seeder
+        // no se vuelva lento.
         $password = Hash::make('123456');
 
-        User::create([
+        $admin = Admin::create(['credential_number' => '0987654321']);
+        $admin->user()->create([
             'name' => 'Administrador',
             'email' => 'admin@prueba.com',
             'password' => $password,
+            'role' => 'ROLE_ADMIN'
         ]);
 
-        //Generar algunos usuarios para nuestra aplicacion
-        for ($i = 0; $i<10; $i++) {
-            $user = User::create([
-                'name' => $faker->name,
-                'email' => $faker-> email,
-                'password' => $password
+        // Generar algunos usuarios para nuestra aplicacion
+        for ($i = 0; $i < 10; $i++) {
+            $writer = Writer::create([
+                'editorial' => $faker->company,
+                'short_bio'=> $faker->paragraph
             ]);
-        }
 
-        $user->categories()->saveMany(
-            $faker->randomElements(
-                array(
-                    Category::find(1),
-                    Category::find(2),
-                    Category::find(3)
-                ), $faker->numberBetween(1, 3), false)
-        );
+            $writer->user()->create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+
+            $writer->user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ), $faker->numberBetween(1, 3), false)
+            );
+        }
     }
 }
